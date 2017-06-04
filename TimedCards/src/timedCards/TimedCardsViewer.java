a602b6bd50eac0658fe0af73a6b51c688d86bd9f
cayuse <cayuse@mac.com>
2017-06-04 13:52:40 -0700

    starts view

--- TimedCards/src/timedCards/TimedCardsViewer.java
+++ TimedCards/src/timedCards/TimedCardsViewer.java
@@ -1,6 +1,20 @@
 package timedCards;
 
-public class TimedCardsViewer
+import java.awt.*;
+import java.util.Random;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+public class TimedCardsViewer implements ActionListener
 {
 
+   /* required for ActionListner, this method is called
+    * when registered actions (buttons etc) are interacted with.
+    */
+   public void actionPerformed(ActionEvent e)
+   {
+   }
 }
